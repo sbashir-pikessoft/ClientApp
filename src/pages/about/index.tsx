be1bb52ba877dd7ed9ba919c0dbf6c52ba7d3d34
Shahzad Bashir <shahzad.bashir@pikessoft.com>
2023-05-02 15:54:08 +0500

    init

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const About = () => {
+    return (
+        <>
+            <div>About Page</div>
+            <div>
+                <Link href={"/"}>Go to Home Page</Link>
+            </div>
+        </>
+    )
+}
+export default About;
